0eaae016cb59c1e3eaf311f2897938b2a119e3ef
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-09-10 10:27:35 +0100

    fixed leagues controller

--- src/main/java/com/qa/controllers/LeaguesController.java
+++ src/main/java/com/qa/controllers/LeaguesController.java
@@ -1,7 +1,7 @@
 package com.qa.controllers;
 
-import com.qa.models.Team;
-import com.qa.repository.TeamsRepository;
+import com.qa.models.League;
+import com.qa.repository.LeaguesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,29 +12,29 @@ import java.util.List;
 public class LeaguesController {
 
     @Autowired
-    private TeamsRepository repository;
+    private LeaguesRepository repository;
 
-    @RequestMapping(value = "teams", method = RequestMethod.GET)
-    public List<Team> listAllTeams() {
+    @RequestMapping(value = "leagues", method = RequestMethod.GET)
+    public List<League> listAllLeagues() {
         return repository.findAll();
     }
-    @RequestMapping(value = "teams", method = RequestMethod.POST)
-    public Team addTeam(@RequestBody Team team) {
-        return repository.saveAndFlush(team);
+    @RequestMapping(value = "leagues", method = RequestMethod.POST)
+    public League addLeague(@RequestBody League league) {
+        return repository.saveAndFlush(league);
     }
-    @RequestMapping(value = "teams/{id}", method = RequestMethod.GET)
-    public Team getTeam(@PathVariable Long id) {
+    @RequestMapping(value = "leagues/{id}", method = RequestMethod.GET)
+    public League getLeague(@PathVariable Long id) {
         return repository.findOne(id);
     }
-    @RequestMapping(value = "teams/{id}", method = RequestMethod.DELETE)
-    public Team deleteTeam(@PathVariable Long id) {
-        Team existing = repository.findOne(id);
+    @RequestMapping(value = "leagues/{id}", method = RequestMethod.DELETE)
+    public League deleteLeague(@PathVariable Long id) {
+        League existing = repository.findOne(id);
         repository.delete(existing);
         return existing;
     }
 //    @RequestMapping(value = "notes/{id}", method = RequestMethod.POST)
-//    public Team updateTeam(@PathVariable Long id, @RequestBody Team team) {
-//        Team existing = repository.findOne(id);
+//    public League updateLeague(@PathVariable Long id, @RequestBody League league) {
+//        League existing = repository.findOne(id);
 //        repository.findOne(id);
 //
 //    }
--- src/main/java/com/qa/repository/LeaguesRepository.java
+++ src/main/java/com/qa/repository/LeaguesRepository.java
@@ -1,10 +1,10 @@
 package com.qa.repository;
 
-import com.qa.models.Team;
+import com.qa.models.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeaguesRepository
-        extends JpaRepository<Team, Long> {
+        extends JpaRepository<League, Long> {
 
 
 
