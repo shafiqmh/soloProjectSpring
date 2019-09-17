a126b147b777e8f31b68d0086c2a17b0a8a9329a
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-09-17 10:58:53 +0100

    commit change to h2

--- src/main/java/com/qa/models/Team.java
+++ src/main/java/com/qa/models/Team.java
@@ -1,11 +1,9 @@
 package com.qa.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="footyteams")
 public class Team {
 
     @Id
